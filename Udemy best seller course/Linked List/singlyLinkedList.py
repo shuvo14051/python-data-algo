bb9da4c6ec514e164972e0233143f7e4019eb390
shuvo14051 <shuvo1137017@gmail.com>
2020-07-28 12:43:51 +0600

    singly Linked list insertion process

--- Udemy best seller course/Linked List/singlyLinkedList.py
+++ Udemy best seller course/Linked List/singlyLinkedList.py
@@ -8,7 +8,40 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert(self, newNode):
+    def listLength(self):
+        currentNode = self.head
+        length = 0
+        while currentNode is not None:
+            length += 1
+            currentNode = currentNode.next
+        return length
+
+    def insertHead(self, newNode):
+        temporaryNode = self.head
+        self.head = newNode
+        self.head.next = temporaryNode
+        del temporaryNode
+
+    def insertAt(self, newNode, position):
+        if position < 0 or position>self.listLength():
+            print("Invalid Position")
+            return
+        if position is 0:
+            self.insertHead(newNode)
+            return
+        currentNode = self.head
+        currentPosition = 0
+        while True:
+            if currentPosition == position:
+                previousNode.next = newNode
+                newNode.next = currentNode
+                break
+
+            previousNode = currentNode
+            currentNode = currentNode.next
+            currentPosition += 1
+
+    def insertEnd(self, newNode):
         #head -> Shuvo ->  None>
         if self.head is None:
             self.head = newNode
@@ -35,14 +68,16 @@ class LinkedList:
 
 
 #firstNode.data = Shuvo, firstNode.next = None
-firstNode = Node("Shuvo")
-secondNode = Node("Labib")
-thirdNode = Node("Sindid")
+firstNode = Node(10)
+secondNode = Node(20)
+thirdNode = Node(15)
+fourthNode = Node(1212)
 
 linkedList = LinkedList()
 
-linkedList.insert(firstNode)
-linkedList.insert(secondNode)
-linkedList.insert(thirdNode)
+linkedList.insertEnd(firstNode)
+linkedList.insertEnd(secondNode)
+linkedList.insertAt(thirdNode, 1)
+linkedList.insertAt(fourthNode, -1)
 
 linkedList.printList()
