470092d7382b6656a128bfa531a0cd1b41b8ddd5
shuvo14051 <shuvo1137017@gmail.com>
2020-07-28 14:37:24 +0600

    x

--- Problem solving/URI/URI-1145.py
+++ Problem solving/URI/URI-1145.py
@@ -1,13 +1,14 @@
 a, b = map(int, input().split())
 x = 0
-if a>b:
-    a,b = b,a
-# if (a > 1 and a < 20) and (b > a and b < 100000):
-if (a > 1 and a < 20) and (b > a and b < 100000):
 
+if ((a > 1 and a < 20) and (b > a and b < 100000)):
+    result = ''
+    final_result = ''
     for i in range(1, b + 1):
-        print("%d " % i, end='')
+        result = result + str(i) +' '
+        final_result += result.strip()
         x += 1
         if x == a:
-            print()
+            result += "\n"
             x = 0
+print(final_result)
